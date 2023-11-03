47dc7a231bc65c13d0d38469f808f00a8262f99b
Hash-Def1ne <xiziya@outlook.com>
2023-11-03 22:23:03 +0800

    change kernel vermagic

--- include/kernel-defaults.mk
+++ include/kernel-defaults.mk
@@ -106,7 +106,8 @@ define Kernel/Configure/Default
 		cp $(LINUX_DIR)/.config.set $(LINUX_DIR)/.config.prev; \
 	}
 	$(_SINGLE) [ -d $(LINUX_DIR)/user_headers ] || $(KERNEL_MAKE) INSTALL_HDR_PATH=$(LINUX_DIR)/user_headers headers_install
-	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
+#	grep '=[ym]' $(LINUX_DIR)/.config.set | LC_ALL=C sort | mkhash md5 > $(LINUX_DIR)/.vermagic
+	echo 5a722da41bc36de95a7195be6fce1b45 > $(LINUX_DIR)/.vermagic
 endef
 
 define Kernel/Configure/Initramfs
